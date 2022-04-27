0a3ea24c907cc5a10fe83a833b4451dbee96f96f
Dark98st <89790659+Dark98st@users.noreply.github.com>
2022-04-27 20:58:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'HELLO!',
-    introDesc: `Đọc cái này đi tuy không giúp bạn hết buồn nhưng đỡ được phần nào`,
+    introDesc: `Đọc cái này đi  tui làm cho mí người nè`,
     btnIntro: ':3 :3',
     title: 'Đọc tới đây rồi thì đọc tiếp đê�:<',
     desc: 'Tại thấy không giúp được gì nên làm cái này cho nó vui vui ',
     btnYes: 'Đọc tiếp vậy',
     btnNo: 'Không, Lười không đọc :3',
-    question:'Tốt coi đến đây thì hi vọng bạn sẽ đỡ , dù không biết xảy ra chuyện gi nhưng "Tối rồi buồn dị sao mà ngủuuuuuuu" ',
+    question:'Tốt.. Tuy mới nch đc gần 2 tháng thui nhưng tui thích mí người là thiệt..Muốn đặt đồ ăn cho mí người cũng là thiệt.Tui sẽ kiên trì cố gắng để tán dc mí người" ',
     btnReply: 'Gửi cho tui :>',
     reply: 'Bớt rảnh lại đi nha.. à không oki cảm ơn nha :v',
-    mess: 'kk Tui biết tui rảnh mà. rảnh đối với mấy người tui thấy nên rảnh thôi, Nên là "Đừng buồn nữa.. Hãy vui lên :v',
-    messDesc: 'Đọc xong thì đi ngủ sớm đi nghen',
+    mess: 'kk Tui biết tui rảnh mà. rảnh đối với mấy người tui thấy nên rảnh thôi, Nên là "Đừng Có mà đẩy tui nữa... tiếp nhận đii mà :< :v',
+    messDesc: 'Đọc xong thì đi học bài nhá.. Mong dc call vs mí nguời nhìu nhìu',
     btnAccept: 'Okiiiii lun :3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
